8280380daefeb0eb19ec3a9ff2715a224752d809
Christian Dreier <dreier.christian@googlemail.com>
2019-05-21 13:26:39 +0200

    added basic cors

--- snippets/chi.go
+++ snippets/chi.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 // ChiFileServer is the fileserver snippet from awesome https://github.com/go-chi/chi
@@ -24,4 +25,18 @@ func ChiFileServer(r chi.Router, path string, root http.FileSystem) {
 	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fs.ServeHTTP(w, r)
 	}))
+}
+
+// BasicCors returns the default cors options suggested in https://github.com/go-chi/cors
+func BasicCors() *cors.Cors {
+	return cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
 }
